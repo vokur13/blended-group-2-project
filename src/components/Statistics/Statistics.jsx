3825e8c7706a2f50d8fbd3ebcee42a66fc33c5fc
AllaYukhym <yukhymenko.alla@gmail.com>
2022-11-26 15:07:50 +0100

    add statistics list

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,10 +4,27 @@ import { FaRegThumbsUp } from 'react-icons/fa';
 import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
 import { GiTreeDoor } from 'react-icons/gi';
 
-export const Statistics = () => {
+const icons = [
+  <FaRegThumbsUp />,
+  <MdPeople />,
+  <MdOutlineProductionQuantityLimits />,
+  <GiTreeDoor />,
+];
+
+export const Statistics = ({ title, stats }) => {
   return (
     <>
-      <div>StatisticsList</div>
+      {title && <StatisticTitle>{title}</StatisticTitle>}
+      <StatisticsList>
+        {stats.map(({ id, total, title }, index) => (
+          <StatisticItem
+            key={id}
+            total={total}
+            title={title}
+            icon={icons[index]}
+          />
+        ))}
+      </StatisticsList>
     </>
   );
 };
